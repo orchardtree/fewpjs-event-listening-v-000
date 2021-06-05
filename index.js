d4a4cbc4a6d3f149a7cd24bd60446c156a376bad
Jesse Wenger <wengerj@gmail.com>
2021-06-05 22:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ function addingEventListener() {
   alert('I was clicked!');
   });
 }
-
-const input = document.getElementById('input');
-input.addEventListener('click', function(event) {
-alert('I was clicked!');
-});
